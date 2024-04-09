981890c819c1714a087813d6cafbedede0f64c9b
Victor Mendizabal <v.mendizabal@alumnos.upm.es>
2024-04-09 23:59:03 +0200

    V3 Jukebox Cabecera y doxygen de port_buzzer.h y port_buzzer.c

--- port/stm32f4/include/port_buzzer.h
+++ port/stm32f4/include/port_buzzer.h
@@ -1,32 +1,79 @@
 /**
  * @file port_buzzer.h
  * @brief Header for port_buzzer.c file.
- * @author alumno1
- * @author alumno2
- * @date fecha
+ * @author Rafael Horcas Mateo (r.horcasm@alumnos.upm.es)
+ * @author Victor Mendizabal Gimeno (v.mendizabal@alumnos.upm.es)
+ * @date 09/04/2024
  */
 #ifndef PORT_BUZZER_H_
 #define PORT_BUZZER_H_
 
 /* Includes ------------------------------------------------------------------*/
 /* Standard C includes */
-
+#include <stdint.h>
+#include <stdbool.h>
 
 /* HW dependent includes */
-
+#include "port_system.h"
 
 /* Defines and enums ----------------------------------------------------------*/
 /* Defines */
-
+#define 	BUZZER_0_ID
+#define 	BUZZER_0_GPIO
+#define 	BUZZER_0_PIN
+#define 	BUZZER_PWM_DC
 
 /* Typedefs --------------------------------------------------------------------*/
-
+typedef struct
+{
+    GPIO_TypeDef *p_port;   /*!<GPIO where the button is connected*/
+    uint8_t pin;            /*!<Pin where the button is connected*/
+    uint8_t alt_func;       /*!<Alternate function value for PWM according to the Alternate function table of the datasheet*/
+    bool note_end           /*!<Flag to indicate that the note has ended*/
+} port_buzzer_hw_t;         
 
 /* Global variables */
-
+extern port_buzzer_hw_t buzzers_arr[];
 
 /* Function prototypes and explanation -------------------------------------------------*/
 
+/**
+ * @brief Retrieve the status of the note end flag.
+ * 
+ * @returns true
+ * @returns false
+ */
+bool port_buzzer_get_note_timeout(uint32_t 	buzzer_id);
+
+/**
+ * @brief Configure the HW specifications of a given buzzer melody player.
+ * 
+ * @param buzzer_id	Buzzer melody player ID. This index is used to select the element of the buzzers_arr[] array
+ */
+void port_buzzer_init(uint32_t buzzer_id);
+
+/**
+ * @brief Set the duration of the timer that controls the duration of the note.
+ * 
+ * @param buzzer_id	Buzzer melody player ID. This index is used to select the element of the buzzers_arr[] array
+ * @param duration_ms	Duration of the note in ms
+ */
+void port_buzzer_set_note_duration(uint32_t buzzer_id, uint32_t duration_ms);
+
+/**
+ * @brief Set the PWM frequency of the timer that controls the frequency of the note.
+ * 
+ * @param buzzer_id	Buzzer melody player ID. This index is used to select the element of the buzzers_arr[] array
+ * @param frequency_hz Frequency of the note in Hz
+ */
+void port_buzzer_set_note_frequency(uint32_t buzzer_id, double frequency_hz);		
+
+/**
+ * @brief Disable the PWM output of the timer that controls the frequency of the note and the timer that controls the duration of the note.
+ * 
+ * @param buzzer_id	Buzzer melody player ID. This index is used to select the element of the buzzers_arr[] array
+ */
+void port_buzzer_stop(uint32_t buzzer_id);
 
 
 #endif
--- port/stm32f4/src/port_buzzer.c
+++ port/stm32f4/src/port_buzzer.c
@@ -1,19 +1,33 @@
 /**
  * @file port_buzzer.c
  * @brief Portable functions to interact with the Buzzer melody player FSM library.
- * @author alumno1
- * @author alumno2
- * @date fecha
+ * @author Rafael Horcas Mateo (r.horcasm@alumnos.upm.es)
+ * @author Victor Mendizabal Gimeno (v.mendizabal@alumnos.upm.es)
+ * @date 09/04/2024
  */
 /* Includes ------------------------------------------------------------------*/
 /* Standard C libraries */
+#include <math.h>
 
 /* HW dependent libraries */
+#include "port_buzzer.h"
 
 /* Global variables */
+#define   ALT_FUNC2_TIM3
 
+
+// Buscar en el datasheet alt_func
+port_buzzer_hw_t buzzers_arr[]= {
+    [BUZZER_0_ID] = {.p_port = BUZZER_0_GPIO, .pin = BUZZER_0_PIN, .alt_func = 0x00, .note_end = false},
+};
 
 /* Private functions */
+
+/**
+ * @brief Configure the timer that controls the duration of the note.
+ * 
+ * @param buzzer_id	Buzzer melody player ID. This index is used to select the element of the buzzers_arr[] array
+ */
 static void _timer_duration_setup(uint32_t buzzer_id)
 {
   if (buzzer_id == BUZZER_0_ID)
@@ -28,18 +42,42 @@ static void _timer_duration_setup(uint32_t buzzer_id)
   }
 }
 
+/**
+ * @brief Configure the timer that controls the PWM of the buzzer.
+ * 
+ * @param buzzer_id	Buzzer melody player ID. This index is used to select the element of the buzzers_arr[] array
+ */
+static void _timer_pwm_setup(uint32_t buzzer_id){
+
+}	
+
 
 
 
 
 /* Public functions -----------------------------------------------------------*/
 
-
-
 void port_buzzer_init(uint32_t buzzer_id)
 {
   port_buzzer_hw_t buzzer = buzzers_arr[buzzer_id];
 
   /* TO-DO alumnos */
+
+}
+
+bool port_buzzer_get_note_timeout(uint32_t buzzer_id){
+
+}
+
+void port_buzzer_set_note_duration(uint32_t buzzer_id, uint32_t duration_ms){
+
+}
+
+
+void port_buzzer_set_note_frequency	(	uint32_t buzzer_id, double frequency_hz){
+
+}
+
+void port_buzzer_stop(uint32_t buzzer_id){
 
 }
